d20d43c6226189f39c2901884ba156569d3149bc
Rikazavr <risempai@gmail.com>
2017-02-08 19:41:03 +0000

    safe version before Copy break anything

---
+++ source/contact.php
@@ -0,0 +1,41 @@
+<?php
+
+$EmailFrom = "quest@gmail.com";
+$EmailTo = "quest@gmail.com";
+$Subject = Trim(stripslashes($_POST['subject']));
+$name = Trim(stripslashes($_POST['name'])); 
+$mail = Trim(stripslashes($_POST['email'])); 
+$message = Trim(stripslashes($_POST['message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Имя: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Почтовый ящик: ";
+$Body .= $mail;
+$Body .= "\n";
+$Body .= "Сообщение: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+/* Do work */
+  if(isset($_REQUEST["destination"])){
+      header("Location: {$_REQUEST["destination"]}");
+  }else if(isset($_SERVER["HTTP_REFERER"])){
+      header("Location: {$_SERVER["HTTP_REFERER"]}");
+  }else{
+       /* some fallback, maybe redirect to index.php */
+  }
+  
+?>
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,48 +1,46 @@
-// This is where it all goes :)
-
 $(document).ready(function ($) {
 
-  setInterval(function () {
-        moveRight();
-    }, 8000);
-  
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
-	
-	$('#slider').css({ width: slideWidth, height: slideHeight });
-	
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    $('#control_prev').click(function () {
-        moveLeft();
-        console.log("sup");
-    });
-
-    $('#control_next').click(function () {
-        moveRight();
-        console.log("pus");
+    //hamburger
+    
+    $(function(){
+      var speed   = 300,
+          button  = $('#menu-button'),
+          overlay = $('#overlay'),
+          menu    = $('#hamburger-menu');
+    
+      button.on('click',function(e){
+        if(overlay.hasClass('open')) {
+          animate_menu('close');
+        } else {
+          animate_menu('open');
+        }
+      });
+    
+      overlay.on('click', function(e){
+        if(overlay.hasClass('open')) {
+          animate_menu('close');
+        }
+      });
+    
+      $('a[href="#"]').on('click', function(e){
+        e.preventDefault();
+      });
+    
+      function animate_menu(menu_toggle) {
+        if(menu_toggle == 'open') {
+          overlay.addClass('open');
+          button.addClass('on');
+          overlay.animate({opacity: 1}, speed);
+          menu.animate({width: "100%", height: "100vh"}, speed);
+        }
+    
+        if(menu_toggle == 'close') {
+          button.removeClass('on');
+          overlay.animate({opacity: 0}, speed);
+          overlay.removeClass('open');
+          menu.animate({width: "0", height: 0}, speed);
+        }
+      }
     });
 
 });
